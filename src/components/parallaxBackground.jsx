e1066567a285b12bd0fd7dc92a5c52f9f14c2a45
naufalprofile <157494382+naufalprofile@users.noreply.github.com>
2025-06-23 13:30:13 +0700

    Update parallaxBackground.jsx

--- src/components/parallaxBackground.jsx
+++ src/components/parallaxBackground.jsx
@@ -1,16 +1,18 @@
-import { motion, useScroll, useSpring, useTransform } from "motion/react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const ParallaxBackground = () => {
   const { scrollYProgress } = useScroll();
-  const x = useSpring(scrollYProgress, { damping: 50 });
-  const mountain3Y = useTransform(x, [0, 0.5], ["0%", "70%"]);
-  const planetsX = useTransform(x, [0, 0.5], ["0%", "-20%"]);
-  const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"]);
-  const mountain1Y = useTransform(x, [0, 0.5], ["0%", "0%"]);
+  const smoothScroll = useSpring(scrollYProgress, { damping: 50, stiffness: 100 });
+
+  const mountain3Y = useTransform(smoothScroll, [0, 1], ["0%", "70%"]);
+  const planetsX = useTransform(smoothScroll, [0, 1], ["0%", "-20%"]);
+  const mountain2Y = useTransform(smoothScroll, [0, 1], ["0%", "30%"]);
+  const mountain1Y = useTransform(smoothScroll, [0, 1], ["0%", "0%"]);
 
   return (
     <section className="absolute inset-0 bg-black/40">
       <div className="relative h-screen overflow-y-hidden">
+
         {/* Background Sky */}
         <div
           className="absolute inset-0 w-full h-screen -z-50"
@@ -18,8 +20,10 @@ const ParallaxBackground = () => {
             backgroundImage: "url(/assets/sky.jpg)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
           }}
         />
+
         {/* Mountain Layer 3 */}
         <motion.div
           className="absolute inset-0 -z-40"
@@ -27,9 +31,11 @@ const ParallaxBackground = () => {
             backgroundImage: "url(/assets/mountain-3.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
             y: mountain3Y,
           }}
         />
+
         {/* Planets */}
         <motion.div
           className="absolute inset-0 -z-30"
@@ -37,9 +43,11 @@ const ParallaxBackground = () => {
             backgroundImage: "url(/assets/planets.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
             x: planetsX,
           }}
         />
+
         {/* Mountain Layer 2 */}
         <motion.div
           className="absolute inset-0 -z-20"
@@ -47,16 +55,19 @@ const ParallaxBackground = () => {
             backgroundImage: "url(/assets/mountain-2.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
             y: mountain2Y,
           }}
         />
-        {/* Mountaine Layer 1 */}
+
+        {/* Mountain Layer 1 */}
         <motion.div
           className="absolute inset-0 -z-10"
           style={{
             backgroundImage: "url(/assets/mountain-1.png)",
             backgroundPosition: "bottom",
             backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
             y: mountain1Y,
           }}
         />
